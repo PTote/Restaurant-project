5aeef8997f7ca7d0ae33e5f5c262b5d678528674
kposadas <kposadas@bytesw.com>
2022-12-01 23:57:20 -0600

    insert order into database v2

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import { MenuService } from 'src/app/services/menu.service';
-
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { collection, addDoc } from 'firebase/firestore'
 
 
 // declare function testeo(): any;
@@ -26,7 +28,7 @@ import { MenuService } from 'src/app/services/menu.service';
 })
 export class FormComponent implements OnInit {
 
-
+  order: Array<any> = []
 
   orderForm = this.fb.group({
     name: new FormControl(''),
@@ -50,22 +52,20 @@ export class FormComponent implements OnInit {
 
   ngOnInit(): void {
     this.getPayment();
+    const order = this.menuService.checkData();
+    this.order = order;
+    console.log(order)
+    // this.test();
   }
 
 
   getPayment() {
     const data = this.menuService.sendTotalPayment();
+    console.log(data)
   }
 
   onSubmit() {
-    console.log(this.orderForm.value)
-    const order = this.menuService.checkData();
-    let total: number = 0;
-    this.menuService.sendTotalPayment().forEach(data => {
-      total = data
-    });
-    const { name, email, address, phoneNumber, paymentMethod, comment } = this.orderForm.value;
-    // sendinfo(name, email, address, phoneNumber, paymentMethod, order, total, comment);
+    this.test();
   }
 
   // addNewUser(name: string = 'kevyn', email: string = '', address: string, phoneNumber: string) {
@@ -73,7 +73,42 @@ export class FormComponent implements OnInit {
 
   // }
 
+  async test() {
+    let total: number = 0;
+    this.menuService.sendTotalPayment().forEach(data => {
+      total = data
+    });
+    const firebaseConfig = {
+      apiKey: "AIzaSyDw5eDEmZNHWFXY-E0rsn4ROwgpJy8PZ54",
+      authDomain: "myrest-1228a.firebaseapp.com",
+      projectId: "myrest-1228a",
+      storageBucket: "myrest-1228a.appspot.com",
+      messagingSenderId: "40994468531",
+      appId: "1:40994468531:web:1a2aa72369df836dbab6c6",
+      measurementId: "G-SVV7PT9G7E"
+    };
 
+    const app = initializeApp(firebaseConfig);
+    const db = getFirestore(app);
+
+    const date = new Date();
+    const orderDate = date.toLocaleString();
+
+
+    const data = {
+      orderDate: orderDate,
+      clientInfo: this.orderForm.value,
+      order: this.order,
+      total: total
+    }
+
+    const docRef = await addDoc(collection(db, "orders"), data);
+    if (docRef) {
+      this.orderForm.reset()
+    }
+    console.log("Document written with ID: ", docRef.id);
+    console.log('Payment Detail:', data)
+  }
 
 
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,17 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  firebaseConfig: {
-    apiKey: "AIzaSyCQMF6gSC6eB-zNstpjjUfjDR7Gfdhpgpk",
-    authDomain: "myrestaurant-158ca.firebaseapp.com",
-    projectId: "myrestaurant-158ca",
-    databaseURL: "https://myrestaurant-158ca-default-rtdb.firebaseio.com/",
-    storageBucket: "myrestaurant-158ca.appspot.com",
-    messagingSenderId: "742839422891",
-    appId: "1:742839422891:web:35e07e3bb2f208242b8cee",
-    measurementId: "G-TN5243W30T"
-  }
+  production: false
 };
 
 /*
